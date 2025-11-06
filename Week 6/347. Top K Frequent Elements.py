5eb4a99c8ec4845df664333736ecfa970507f450
Rohit Shidid <rrs6770@nyu.edu>
2025-11-06 17:01:07 -0500

    Week 6

---
+++ Week 6/347. Top K Frequent Elements.py
@@ -0,0 +1,7 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        return [num for num, _ in heapq.nlargest(k, freq.items(), key=lambda x: x[1])]
